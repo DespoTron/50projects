5f229ea57d39a10804d270c983c7df9d009d4de3
Thai Do <thai.hl.do@gmail.com>
2020-12-12 16:01:08 -0800

    Fixed bug missing bracket

--- Form_Wave/script.js
+++ Form_Wave/script.js
@@ -1,5 +1,11 @@
 const labels = document.querySelectorAll('.form-control label')
 
-labels.forEach(label => {
-  label.innerHTML = label.innerText.split('').map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`).join('')
-}
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map(
+      (letter, index) =>
+        `<span style="transition-delay:${index * 50}ms">${letter}</span>`
+    )
+    .join('')
+})
