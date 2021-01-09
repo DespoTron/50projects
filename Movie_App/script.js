3b01bb446ba138e680fa1072ccccad08a662b2fe
Thai Do <thai.hl.do@gmail.com>
2021-01-08 21:31:50 -0800

    Add config.js file to hide keys

--- Movie_App/script.js
+++ Movie_App/script.js
@@ -1,7 +1,31 @@
-const API_URL =
-  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=64c415fa84b1a155b07c6b0e385b9dff&page=1'
+const API_URL = require('./config')
+const SEARCH_API = require('./config')
+const IMG_PATH = require('./config')
 
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
-const SEARCH_URL =
-  'https://api.themoviedb.org/3/search/movie?api_key=64c415fa84b1a155b07c6b0e385b9dff&query="'
+// Get initial movies
+getMovies(API_URL)
+
+async function getMovies(url) {
+  const res = await fetch(url)
+
+  const data = await res.json()
+
+  console.log(data.results)
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search.value
+
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm)
+
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})
