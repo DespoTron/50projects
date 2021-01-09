ba69f052ffa2770425730d89a0e338e300020418
Thai Do <thai.hl.do@gmail.com>
2021-01-08 21:44:53 -0800

    Have to figure out config.js settings

--- Movie_App/script.js
+++ Movie_App/script.js
@@ -1,6 +1,10 @@
-const API_URL = require('./config')
-const SEARCH_API = require('./config')
-const IMG_PATH = require('./config')
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=64c415fa84b1a155b07c6b0e385b9dff&page=1'
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=64c415fa84b1a155b07c6b0e385b9dff&query="'
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
