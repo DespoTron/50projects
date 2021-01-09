b997c3c4b754a351e62cd7c106fecfa35860c944
Thai Do <thai.hl.do@gmail.com>
2021-01-08 21:19:38 -0800

    Setup account on tmdb

---
+++ Movie_App/script.js
@@ -0,0 +1,7 @@
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=64c415fa84b1a155b07c6b0e385b9dff&page=1'
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+
+const SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=64c415fa84b1a155b07c6b0e385b9dff&query="'
