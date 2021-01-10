4a3b095744a9aefcbf7d2fbb3dd046cf0074b65f
Thai Do <thai.hl.do@gmail.com>
2021-01-09 16:02:42 -0800

    Completed simple JS code

--- Background_Slider/script.js
+++ Background_Slider/script.js
@@ -5,6 +5,28 @@ const rightBtn = document.getElementById('right')
 
 let activeSlide = 0
 
+rightBtn.addEventListener('click', () => {
+  activeSlide++
+
+  if (activeSlide > slides.length - 1) {
+    active = 0
+  }
+
+  setBgToBody()
+  setActiveSlide()
+})
+
+leftBtn.addEventListener('click', () => {
+  activeSlide--
+
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1
+  }
+
+  setBgToBody()
+  setActiveSlide()
+})
+
 setBgToBody()
 
 function setBgToBody() {
