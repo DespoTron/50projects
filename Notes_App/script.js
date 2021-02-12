3329b7ea5f2f4788d804c02349828935d030ebaf
Thai Do <thai.hl.do@gmail.com>
2021-02-11 21:12:22 -0800

    Added feature to save data to local storage

--- Notes_App/script.js
+++ Notes_App/script.js
@@ -1,10 +1,18 @@
 const addBtn = document.getElementById('add')
 
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if (notes) {
+  notes.forEach((note) => {
+    addNewNote(note)
+  })
+}
+
 addBtn.addEventListener('click', () => {
   addNewNote()
 })
 
-function addNewNote(text = 'Hello world') {
+function addNewNote(text = '') {
   const note = document.createElement('div')
   note.classList.add('note')
 
@@ -28,6 +36,8 @@ function addNewNote(text = 'Hello world') {
 
   deleteBtn.addEventListener('click', () => {
     note.remove()
+
+    updateLS()
   })
 
   editBtn.addEventListener('click', () => {
@@ -39,7 +49,21 @@ function addNewNote(text = 'Hello world') {
     const { value } = e.target
 
     main.innerHTML = marked(value)
+
+    updateLS()
   })
 
   document.body.appendChild(note)
+}
+
+function updateLS() {
+  const notesText = document.querySelectorAll('textarea')
+
+  const notes = []
+
+  notesText.forEach((note) => {
+    notes.push(note.value)
+  })
+
+  localStorage.setItem('notes', JSON.stringify(notes))
 }
